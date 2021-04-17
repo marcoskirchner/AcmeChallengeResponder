08550936900f1d99e5dc2b2e9b377e5f4155ba49
Marcos Kirchner <marcoskirchner@live.com>
2021-04-17 19:25:03 -0300

    Refactoring the endpoints. All the code was previously in Startup.cs, using route to code style endpoints. The route to code is still in use but the actual code has been moved to another class.

---
+++ EndpointHandlers.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcmeChallengeRestResponder
+{
+    static class EndpointHandlers
+    {
+        private static readonly Dictionary<string, string> _challengeTokens = new();
+
+        internal static async Task HandleGet(HttpContext context)
+        {
+            var currentUrl = context.Request.GetDisplayUrl();
+            if (_challengeTokens.TryGetValue(currentUrl, out var token))
+            {
+                context.Response.StatusCode = StatusCodes.Status200OK;
+                await context.Response.WriteAsync(token);
+            }
+            else
+            {
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+            }
+        }
+
+        internal static async Task HandlePut(HttpContext context)
+        {
+            var contentLength = context.Request.ContentLength;
+            if (!contentLength.HasValue || contentLength.Value <= 0 || context.Request.ContentLength > 500)
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                return;
+            }
+
+            var currentUrl = context.Request.GetDisplayUrl();
+
+            var buffer = new byte[(int)contentLength];
+            var bytesRead = await context.Request.Body.ReadAsync(buffer, 0, (int)contentLength);
+            var token = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            _challengeTokens[currentUrl] = token;
+            context.Response.StatusCode = StatusCodes.Status201Created;
+        }
+
+        internal static Task HandleDelete(HttpContext context)
+        {
+            var currentUrl = context.Request.GetDisplayUrl();
+            if (_challengeTokens.ContainsKey(currentUrl))
+            {
+                _challengeTokens.Remove(currentUrl);
+                context.Response.StatusCode = StatusCodes.Status204NoContent;
+            }
+            else
+            {
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,22 +2,14 @@ using AcmeChallengeRestResponder.Security.SimpleBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AcmeChallengeRestResponder
 {
     public class Startup
     {
-        const string PATH_PATTERN = "{**path}";
-        private readonly Dictionary<string, string> _challengeTokens = new();
-
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -54,51 +46,11 @@ namespace AcmeChallengeRestResponder
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapPut(PATH_PATTERN, async context =>
-                {
-                    var contentLength = context.Request.ContentLength;
-                    if (!contentLength.HasValue || contentLength.Value <= 0 || context.Request.ContentLength > 500)
-                    {
-                        context.Response.StatusCode = StatusCodes.Status400BadRequest;
-                        return;
-                    }
-
-                    var currentUrl = context.Request.GetDisplayUrl();
-
-                    var buffer = new byte[(int)contentLength];
-                    var bytesRead = await context.Request.Body.ReadAsync(buffer, 0, (int)contentLength);
-                    var token = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-
-                    _challengeTokens[currentUrl] = token;
-                    context.Response.StatusCode = StatusCodes.Status201Created;
-                });
-                endpoints.MapGet(PATH_PATTERN, async context =>
-                {
-                    var currentUrl = context.Request.GetDisplayUrl();
-                    if (_challengeTokens.TryGetValue(currentUrl, out var token))
-                    {
-                        context.Response.StatusCode = StatusCodes.Status200OK;
-                        await context.Response.WriteAsync(token);
-                    }
-                    else
-                    {
-                        context.Response.StatusCode = StatusCodes.Status404NotFound;
-                    }
-                }).AllowAnonymous();
-                endpoints.MapDelete(PATH_PATTERN, context =>
-                {
-                    var currentUrl = context.Request.GetDisplayUrl();
-                    if (_challengeTokens.ContainsKey(currentUrl))
-                    {
-                        _challengeTokens.Remove(currentUrl);
-                        context.Response.StatusCode = StatusCodes.Status204NoContent;
-                    }
-                    else
-                    {
-                        context.Response.StatusCode = StatusCodes.Status404NotFound;
-                    }
-                    return Task.CompletedTask;
-                });
+                const string PATH_PATTERN = "{**path}";
+                
+                endpoints.MapGet(PATH_PATTERN, EndpointHandlers.HandleGet).AllowAnonymous(); /* the GET endpoint MUST allow anonymous access */
+                endpoints.MapPut(PATH_PATTERN, EndpointHandlers.HandlePut);
+                endpoints.MapDelete(PATH_PATTERN, EndpointHandlers.HandleDelete);
             });
         }
     }
